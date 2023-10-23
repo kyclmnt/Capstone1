5efb49730fa4bb559d34745df3df5db94a780855
kyclmnt <clementealliahkaye@gmail.com>
2023-10-23 21:01:50 +0800

    fixed login page

---
+++ OES/dashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard</title>
+    <link rel="stylesheet" href="css/dashboard.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+        <div id="logo-holder">
+            <img src="logo.png" alt="" style="width:35px;height:35px";>
+            ABIS
+       </div> 
+      
+        <nav>
+            <a href="dashboard.html">Dashboard</a>
+            <a href="student.html">Students</a>
+            <a href="settings.html">Settings</a>
+            <a href="login.html">Logout</a>
+        </nav>
+   
+    </header>
+    <main>
+        <h1>
+            DASHBOARD
+        </h1>
+    </main>
+</body>
+</html>
